d3d20a6e317b982e842dfcc95b92600b40f21876
Daniel Prado <dfeprado@gmail.com>
2026-04-14 10:21:26 -0300

    Create package anacomb; move factorial function to anacomb/factorial.go file

---
+++ anacomb/factorial.go
@@ -0,0 +1,23 @@
+
+package anacomb
+
+import "errors"
+
+// TODO implements memoization in a way the dev can choose to enable it or disable it
+
+func Factorial(x int) (int, error) {
+	if x < 0 {
+		return 0, errors.New("x must be >= 0")
+	} else if x == 0 {
+		return 1, nil
+	} else if x <= 2 {
+		return x, nil
+	}
+
+	result := 1
+	for x > 1 {
+		result *= x
+		x -= 1
+	}
+	return result, nil
+}
